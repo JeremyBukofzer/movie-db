cc184517df9b80e39027496f02043b04c7ee5a8f
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-11-16 21:17:34 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies (movie_name)
+VALUES ("Inception"),
+       ("Jurassic Park"),
+       ("Finding Nemo");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "A bit confusing."),
+       (2, "Dinosaurs are cool!"),
+       (3, "I liked when the fish swam.");
